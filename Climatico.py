a68e5c870bbf0c55d5b0c206df3322d5d633c588
hectorflores329 <hectorflores329@gmail.com>
2021-05-27 17:49:57 -0600

    Commit CSV

--- Climatico.py
+++ Climatico.py
@@ -25,89 +25,9 @@ def fechaA():
     date = date.strftime('%d-%m-%Y')
     return date
 
-def descargarDatos():
 
-    driver = getDriver()
-    exist = 0
-
-    while (exist == 0):
-        enlace = driver.find_elements_by_xpath("/html/body/main/div/div[1]/div/div/div/div/div/div/div[4]/div/div/div/a")
-        # time.sleep(1)
-
-        if (len(enlace) == 0):
-            exist = 0
-            time.sleep(60)
-        else:
-            exist = 1
-
-    
-    for i in range(len(enlace)):
-        
-        namefile = str(i+1) + ". Clima " +  fechaA()
-        print(enlace[i].text)
-        
-        df = pd.read_csv(enlace[i].text)
-
-        df["Fecha actual"] = fechaA()
-        del df["system:index"]
-        del df["COMUNA"]
-        del df["Malla16k"]
-        del df["Malla1k"]
-        del df["Malla2k"]
-        del df["Malla4k"]
-        del df["Malla8k"]
-        del df["NOM_COMUNA"]
-        del df["NOM_PROVIN"]
-        del df["NOM_REGION"]
-        del df["PROVINCIA"]
-        del df["REGION"]
-        del df["latitude"]
-        del df["longitude"]
-        del df[".geo"]
-
-        df.to_excel("Clima/"  + str(namefile) +".xlsx", index=False)
-
-    driver.close()
-
-def unificar():
-    file = "Clima/*.xlsx"
-    files = glob.glob(file)
-
-    archivos = np.array(files)
-    final = "Clima/Consolidado.csv"
-    
-    df_inicial = pd.read_csv(final)
-    df_inicial
-    
-    _df = df_inicial[df_inicial["Fecha actual"] == ""]
-    _df.to_csv("Clima/Consolidado.csv", index=False)
-
-    for i in range(len(files)):
-        df_inicial = pd.read_csv(final)
-        df_inicial
-
-        if(str(archivos[i])!=final):
-            df = pd.read_excel(archivos[i])
-
-            n = df_inicial.append([df])
-            n.to_csv(final, index=False)
-
-def generarVariable():
-    dfTemp = pd.read_csv("Clima/Consolidado.csv", usecols=("Parcela_ID","Temp_C"))
-    dfHumidity = pd.read_csv("Clima/Consolidado.csv", usecols=("Parcela_ID","relative_humidity_2m_above_ground"))
-    dfWind = pd.read_csv("Clima/Consolidado.csv", usecols=("Parcela_ID","wind"))
-    dfET = pd.read_csv("Clima/Consolidado.csv", usecols=("Parcela_ID","ET"))
-
-    dfTemp.to_excel("Clima/V - Temperatura.xlsx", index=False)
-    dfHumidity.to_excel("Clima/V - Humedad relativa.xlsx", index=False)
-    dfWind.to_excel("Clima/V - Viento.xlsx", index=False)
-    dfWind.to_excel("Clima/V - ET.xlsx", index=False)
 
 if __name__ == '__main__':
     print("Descargando datos...")
-
+    
     descargarDatos()
-    unificar()
-    generarVariable()
-
-    print("Los datos se han descargado")
